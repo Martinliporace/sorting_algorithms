45f8bf5f15587a20568f6f8c20342953d457651b
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-11 17:33:25 -0300

    checkeando el 1

--- Nico/1-insertion_sort_list.c
+++ Nico/1-insertion_sort_list.c
@@ -18,17 +18,19 @@ void insertion_sort_list(listint_t **list)
     while(j)
     {
         i = j->next;
-        while(j->prev && j->n < j->prev->n)
+        while(j->prev)
         {
-            j->prev->next = j->next; /*i next a k*/
-            if(j->next)
-                j->next->prev = j->prev; /*k prev a i*/
-            j->next = j->prev;/*j next a i*/
-            j->prev = j->prev->prev; /*j prev a h*/
-            if(j->prev)
-                j->prev->next = j;/*h next a j*/
-            j->next->prev = j; /*i prev a j*/
-            
+            if(j->n < j->prev->n)
+            {
+                j->prev->next = j->next; /*i next a k*/
+                if(j->next)
+                    j->next->prev = j->prev; /*k prev a i*/
+                j->next = j->prev;/*j next a i*/
+                j->prev = j->prev->prev; /*j prev a h*/
+                if(j->prev)
+                    j->prev->next = j;/*h next a j*/
+                j->next->prev = j; /*i prev a j*/
+            }
             if(!j->prev)
                 *list = j;
             
