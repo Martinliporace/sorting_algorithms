a90b47213820ddc3994975ccf1a385970cc2a5d9
root <martin.liporace@gmail.com>
2022-06-11 05:21:45 -0700

    betty fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
-* Description -  sorts an array of integers in ascending order using the
+* selection_sort -  sorts an array of integers in ascending order using the
 * Selection sort algorithm
 * @array: list of elements to be sort
 * @size: length of array
@@ -12,23 +12,23 @@ void selection_sort(int *array, size_t size)
 size_t i, j, position = 0;
 int aux = 0;
 
-for(i = 0; i < size - 1; i++)
-{
-    j = i + 1;
-    aux = array[i];
-    position = i;
-    while(j < size)
-    {
-        if(array[j] < aux)
-        {
-            aux = array[j];
-            position = j;
-        }
-        j++;
-    }
-    array[position] = array[i];
-    array[i] = aux;
-    if(aux != array[position])
-        print_array(array, size);
-}
+for (i = 0; i < size - 1; i++)
+	{
+	j = i + 1;
+	aux = array[i];
+	position = i;
+	while (j < size)
+		{
+		if (array[j] < aux)
+			{
+			aux = array[j];
+			position = j;
+			}
+		j++;
+		}
+	array[position] = array[i];
+	array[i] = aux;
+	if (aux != array[position])
+		print_array(array, size);
+	}
 }
