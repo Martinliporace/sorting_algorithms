2cc8a88fc0d20fc3ac070cffd29a7d492ce46e92
root <martin.liporace@gmail.com>
2022-06-13 12:27:45 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -23,5 +23,10 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+void swap_array(int *i, int *j);
+int lomuto(int *array, int der, int izq, size_t size);
+void quick(int array[], int low, int high, size_t size);
 
 #endif
